984f8ef6ecdcda20e45fe008a4c2d07385dea2c4
luca <luca@WIN-UTNG2JHFQ6E>
2016-05-03 15:44:02 +0200

    aggiornato

--- src/org/dstech/lulizi/MainPattern.java
+++ src/org/dstech/lulizi/MainPattern.java
@@ -12,7 +12,7 @@ public class MainPattern {
 		boolean risposta=false;
 		do{
 		Scanner inputtast= new Scanner(System.in);
-		System.out.println("Inserisci una stringa (telefono o email)");
+		System.out.println("Inserisci una stringa ( telefono o email)");
 		String input= inputtast.nextLine(); 
 		System.out.println("Hai inserito la stringa "+input);
 		
