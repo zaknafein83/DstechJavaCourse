45f34353e37f66701d09a42e4bc3f655a16b4054
Valerio <Valerio@192.168.7.182>
2016-05-04 13:17:32 +0200

    Secondo rilascio di MyAgend beta 0.2

--- src/org/dstech/vangelini/Agenda/Persona.java
+++ src/org/dstech/vangelini/Agenda/Persona.java
@@ -1,8 +1,6 @@
 package org.dstech.vangelini.Agenda;
 
 import java.util.Date;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Persona {
 	private String nome;
@@ -13,28 +11,12 @@ public class Persona {
 	private Date data;
 	
 	public Persona(String nome, String cognome, Integer eta, String numero_telefono, String email, Date data) {
-		//regola per email
-		String prima_regola = "(^\\w+@\\w+\\.\\w{2,6}$)";
-		//regola per telefono (+39 338 1234567 | +39 3381234567 | 3381234567 | +39 338 123 4567)
-		String seconda_regola = "(^\\d{10}$)";
-		String terza_regola = "(^+39 \\d{3} \\d{3} \\d{4}$)";
-		String quarta_regola = "(^+39 \\d{10}$)";
-		String quinta_regola = "(^+39 \\d{3} \\d{7}$)";
-		
-		Pattern pattern = Pattern.compile(prima_regola);
-		Pattern pattern2 = Pattern.compile(seconda_regola+"|"+terza_regola+"|"+quarta_regola+"|"+quinta_regola);
-		Matcher matcher = pattern.matcher(email);
 		this.nome = nome;
 		this.cognome = cognome;
 		this.eta = eta;
 		this.data = data;
-		if(matcher.find()){
-			this.email = email;
-		}
-		Matcher matcher2 = pattern2.matcher(numero_telefono);
-		if(matcher2.find()){
-			this.numero_telefono = numero_telefono;
-		}
+		this.email = email;
+		this.numero_telefono = numero_telefono;
 	}
 	
 	public Persona(){};
@@ -81,6 +63,10 @@ public class Persona {
 		return "Nome: " + nome + ", Cognome: " + cognome + ", eta: "
 				+ eta + ", Numero Telefonico: " + numero_telefono + ", email: "
 				+ email + ", Data di aggiunta alla rubrica:" + data + "]";
+	}
+	
+	public String toStringFile() {
+		return (nome + "," + cognome + "," + eta + "," + numero_telefono + "," + email + "," + data);
 	}
 	
 	public boolean modificaPersona(String nome, String cognome, Integer eta, String numero_telefono, String email, Date data){
