42274cf8631a59d157be0ebce9b900d20d2a7b40
Gianluca <Gianluca@DESKTOP-RJ986GT>
2016-05-05 14:29:20 +0200

    ff

--- src/org/dstech/fsisca/utility/FormattingExample.java
+++ src/org/dstech/fsisca/utility/FormattingExample.java
@@ -13,8 +13,10 @@
 package org.dstech.fsisca.utility;
 
 import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 /**
  * @author franksisca
@@ -22,20 +24,27 @@ import java.util.Date;
  */
 public class FormattingExample {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
-		SimpleDateFormat dateFormat =  new SimpleDateFormat("dd-MM-yy w");
-		Date newDate = new Date();
+		Scanner scanner = new Scanner (System.in);
+		System.out.println("Inserisci una data nel formato dd/mm/yyyy");
+		SimpleDateFormat dateFormat =  new SimpleDateFormat("dd/mm/yyyy");
+		String data = scanner.next();
+		Date parse = dateFormat.parse(data);
 		
-		System.out.println(newDate);
-		System.out.println(dateFormat.format(newDate));
-		
-		DecimalFormat decimalFormat = new DecimalFormat("#0.00");
-		double value = 23123123.12313521;
-		
-		System.out.println(value);
-		System.out.println(decimalFormat.format(value));
-		
+		System.out.println("la data inserita è: " + parse);
+//		
+//		Date newDate = new Date();
+//		
+//		System.out.println(newDate);
+//		System.out.println(dateFormat.format(newDate));
+//		
+//		DecimalFormat decimalFormat = new DecimalFormat("#0.00");
+//		double value = 23123123.12313521;
+//		
+//		System.out.println(value);
+//		System.out.println(decimalFormat.format(value));
+//		
 		
 	}
 
