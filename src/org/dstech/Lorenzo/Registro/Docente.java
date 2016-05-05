816c9eb11fdc9d328e7bc59a6ed95234000ce7ba
user <user@user-PC>
2016-05-05 16:11:08 +0200

    --

---
+++ src/org/dstech/Lorenzo/Registro/Docente.java
@@ -0,0 +1,29 @@
+package org.dstech.Lorenzo.Registro;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Docente extends Persona{
+	private String matricola;
+
+	public String getMatricola() {
+		return matricola;
+	}
+	public void setMatricola() {
+		SimpleDateFormat dateFormat =  new SimpleDateFormat("dd-MM-yyyy");
+		Calendar cal = Calendar.getInstance();
+		int mese = cal.get(Calendar.MONTH);
+		int anno = cal.get(Calendar.YEAR);
+		//System.out.println(Persona.class.getName()+mese+anno);
+        matricola=(getNome()+mese+anno);
+	}	
+	@Override
+	public String matricola() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+		
+	
+}
