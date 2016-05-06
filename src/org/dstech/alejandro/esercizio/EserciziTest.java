3e0e036147ce6bc16e2dec6a8c5f2d16d9871e27
Alejandro <Alejandro@Hazard>
2016-05-06 11:42:18 +0200

    esercizio ContaPositivi

---
+++ src/org/dstech/alejandro/esercizio/EserciziTest.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package org.dstech.alejandro.esercizio;
+
+import java.util.Scanner;
+
+/**
+ * @author Alejandro
+ *
+ */
+public class EserciziTest{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		contaPositivi();
+
+	}
+	
+	
+	public static void contaPositivi(){
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Inserisci sequenza");
+		String s = sc.nextLine();
+		String b=s.replace("X", "");
+		String[] str=b.split(" ");
+		
+		System.out.println("I numeri positivi sono:");
+		for(String i:str){
+			
+			if(Integer.parseInt(i)>0)
+				System.out.println(i);
+		}
+		
+		
+		
+	}
+
+}
